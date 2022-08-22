97233f0d623dab234aa9a504ea48fb44ce2a2715
NaluFigueira <anachfigueira@gmail.com>
2022-08-21 23:59:22 -0300

    Including devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Posts } from "./Posts";
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 function App() {
   const queryClient = new QueryClient();
@@ -11,6 +13,7 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
