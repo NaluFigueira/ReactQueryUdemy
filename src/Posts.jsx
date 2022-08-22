c0cd11178191a4c65040b9840251ee3a79155729
NaluFigueira <anachfigueira@gmail.com>
2022-08-21 21:03:26 -0300

    Adding handler for loading and error in query

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -15,9 +15,11 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  const { data } = useQuery(['posts'], fetchPosts);
+  const { data, isError, error, isLoading } = useQuery(['posts'], fetchPosts);
 
-  if(!data) return <div />
+  if(isLoading) return <h3>Loading...</h3>;
+
+  if(isError) return <h3>{error.toString()}</h3>
 
   return (
     <>
