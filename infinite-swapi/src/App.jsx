c8579c28f3136fb1388c57b39b3ef1002822a6bb
NaluFigueira <anachfigueira@gmail.com>
2022-09-03 15:16:04 -0300

    Using useInfiniteQuery for InfiniteSpecies component

--- infinite-swapi/src/App.jsx
+++ infinite-swapi/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import { InfinitePeople } from "./people/InfinitePeople";
 import { InfiniteSpecies } from "./species/InfiniteSpecies";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const client = new QueryClient();
@@ -11,8 +11,8 @@ function App() {
     <div className="App">
       <QueryClientProvider client={client}>
         <h1>Infinite SWAPI</h1>
-        <InfinitePeople />
-        {/* <InfiniteSpecies /> */}
+        {/* <InfinitePeople /> */}
+        <InfiniteSpecies />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </div>
--- infinite-swapi/src/species/InfiniteSpecies.jsx
+++ infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,3 +1,4 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import { Species } from "./Species";
 
@@ -8,6 +9,45 @@ const fetchUrl = async (url) => {
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    ["sw-species"],
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next,
+    }
+  );
+
+  if (isLoading) {
+    return <p className="loading">Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error: {error.toString()}</p>;
+  }
+
+  return (
+    <>
+      {isFetching && <p className="loading">Loading...</p>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((specie) => (
+            <Species
+              key={specie.name}
+              name={specie.name}
+              language={specie.language}
+              averageLifespan={specie.average_lifespan}
+            />
+          ));
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
