5982b681861046053673dde68c6c429bb0be6d0d
NaluFigueira <anachfigueira@gmail.com>
2022-09-24 15:14:23 -0300

    Using useMutation and invalidateQueries to cancel appointment

--- lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,12 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import dayjs from 'dayjs';
 
 import type { Appointment, User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useUser } from './useUser';
 
-// for when we need a query function for useQuery
 async function getUserAppointments(
   user: User | null,
 ): Promise<Appointment[] | null> {
